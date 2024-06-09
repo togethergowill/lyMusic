4014d8b4fdde2c2c25de47ac1eae4fc78793e383
HL <HL2972364289@qq.com>
2024-06-09 15:53:21 +0800

    player-bar

---
+++ components/player-bar/index.js
@@ -0,0 +1,22 @@
+// components/player-bar/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    albumUrl: {
+      type: String,
+      value: "https://p1.music.126.net/fYUy7d0yrq-2EqNIzbF42Q==/109951168012301324.jpg",
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+})
